1a947cafa66fbd7999f6b6f1d76ac00adda57a79
Macdaveed <Macdaveedgolden@gmail.com>
2023-12-15 08:43:43 +0100

    add_num

--- add_num.c
+++ add_num.c
@@ -4,14 +4,15 @@
  * add_num - a program that add two numbers
  * @a: first integer
  * @b: second interger
- * @sum: the result from the sum.
  * Return: sum of the two integers.
  */
 
-int add_num(int a, int b, char sum)
+int add_num(int a, int b)
 {
+	int sum;
 
 	sum = a + b;
+
 	printf("sum is: %d\n", sum);
 	return (sum);
 }
